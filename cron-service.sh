ec19e7dcffc631645ddbfb9bd8e3fda35e64edb0
Daniel da Silva <daniel@meltingwax.net>
2012-12-15 09:09:51 +0000

    Last commit before night

--- cron-service.sh
+++ cron-service.sh
@@ -1,44 +1,53 @@
 #!/bin/bash
 
+
+
+if [ "$USER" != "root" ]; then
+    echo "Must be run as root."
+    exit 1
+fi
+
 if [ "$1" == "" ]; then
     echo "$0 port [port [port [...]]]"
     exit 1
-fi
- 
-IPTABLES_TARGET="DROP"
-IPTABLES_CHAINNAME="TOR"
-TMP_TOR_LIST="/tmp/temp_tor_list"
-IP_ADDRESS=$(ifconfig eth0 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')  # eth0 specific
+fi 
 
-# Create the iptables chain if it doesn't exist 
-if [ ! iptables -L "$IPTABLES_CHAINNAME" -n >/dev/null 2>&1 ]
-then
-    iptables -N "$IPTABLES_CHAINNAME" >/dev/null 2>&1
+CHAIN_NAME="TOR"
+TMP_TOR_LIST="/tmp/temp_tor_list"
+IP_ADDRESS=$(ifconfig eth0 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
+
+# Create tor chain if it doesn't exist. This is basically a grouping of
+# filters within iptables.
+if ! iptables -L "$CHAIN_NAME" -n >/dev/null 2>&1 ; then
+    iptables -N "$CHAIN_NAME" >/dev/null 2>&1
 fi
 
 # Download the exist list from the tor project, build the temp file. Also
 # filter out any commented (#) lines.
-rm $TMP_TOR_LIST
+rm -f $TMP_TOR_LIST
 touch $TMP_TOR_LIST
-for port in "$@"
+
+for PORT in "$@"
 do
-    wget -q -O - "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$IP_ADDRESS&port=$port" -U NoSuchBrowser/1.0 >> $TMP_TOR_LIST
+    wget -q -O - "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$IP_ADDRESS&port=$PORT" -U NoSuchBrowser/1.0 >> $TMP_TOR_LIST
     echo >> $TMP_TOR_LIST
 done
 
 sed -i 's|^#.*$||g' $TMP_TOR_LIST
 
-
 # Block the contents of the list in top
+iptables -F $CHAIN_NAME
 
- 
-iptables -F "$IPTABLES_CHAINNAME"
- 
 for IP in $(cat $TMP_TOR_LIST | uniq | sort)
 do
-    iptables -A "$IPTABLES_CHAINNAME" -s $IP -j $IPTABLES_TARGET
+    for PORT in "$@"
+    do
+	iptables -A $CHAIN_NAME -s $IP -p tcp --destination-port $PORT -j DROP
+    done
 done
- 
-iptables -A "$IPTABLES_CHAINNAME" -j RETURN
- 
+
+iptables-save
+
+#echo iptables -A $CHAIN_NAME -j RETURN
+
 
